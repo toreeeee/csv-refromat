a12a5e6d75d5d36cbde03a95fa12a0129c0d48bb
edgar.nentzl <edgar.nentzl@check24.de>
2024-06-24 13:10:20 +0200

    Improved some test messages

--- app/utils/array/array_test.go
+++ app/utils/array/array_test.go
@@ -13,9 +13,9 @@ func TestMap(t *testing.T) {
 		return fmt.Sprintf("%d", *v)
 	})
 
-	assert.Equal(t, len(input), len(output))
-	assert.NotEqual(t, input, output)
-	assert.Equal(t, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, output)
+	assert.Equal(t, len(input), len(output), "map output length should be the same as input; else something went missing")
+	assert.NotEqual(t, input, output, "input shouldn't match output when transformed")
+	assert.Equal(t, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, output, "should match int input as string")
 }
 
 func TestFilter(t *testing.T) {
@@ -24,6 +24,6 @@ func TestFilter(t *testing.T) {
 		return *v > 10
 	})
 
-	assert.NotEqual(t, len(input), len(output))
+	assert.NotEqual(t, len(input), len(output), "output shouldn't match input length after filtering")
 	assert.Equal(t, []int{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, output)
 }
--- app/utils/extra_math/extra_math_test.go
+++ app/utils/extra_math/extra_math_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestMin(t *testing.T) {
-	assert.Equal(t, 1, extra_math.Min(1, 2))
-	assert.Equal(t, 1, extra_math.Min(2, 1))
+	assert.Equal(t, 1, extra_math.Min(1, 2), "min value of 1 and 2 should be 1")
+	assert.Equal(t, 1, extra_math.Min(2, 1), "min value of 2 and 1 should be 1")
 
-	assert.Equal(t, -2, extra_math.Min(-1, -2))
-	assert.Equal(t, -55, extra_math.Min(55, -55))
+	assert.Equal(t, -2, extra_math.Min(-1, -2), "min value of -1 and -2 should be -2")
+	assert.Equal(t, -55, extra_math.Min(55, -55), "min value of 55 and -55 should be -55")
 
-	assert.Equal(t, 1, extra_math.Min(1, 1))
+	assert.Equal(t, 1, extra_math.Min(1, 1), "when both values match output should match both")
 }
